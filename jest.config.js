2a6b0d3cf511db269a47c26dd4f903c6120138ce
David Idol <david.idol@gmail.com>
2019-05-09 21:30:34 -0700

    Remove hardcoded paths

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,14 @@
 module.exports = {
   transform: {
-    '.(ts|tsx)':
-      '/Users/idol/Code/redux-combine-reducers-immutable/node_modules/ts-jest/dist/index.js',
+    '.(ts|tsx)': 'node_modules/ts-jest/dist/index.js',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   testMatch: ['<rootDir>/test/**/*.(spec|test).{ts,tsx}'],
   testURL: 'http://localhost',
-  rootDir: '/Users/idol/Code/redux-combine-reducers-immutable',
   watchPlugins: [
-    '/Users/idol/Code/redux-combine-reducers-immutable/node_modules/jest-watch-typeahead/filename.js',
-    '/Users/idol/Code/redux-combine-reducers-immutable/node_modules/jest-watch-typeahead/testname.js',
+    'node_modules/jest-watch-typeahead/filename.js',
+    'node_modules/jest-watch-typeahead/testname.js',
   ],
 };
